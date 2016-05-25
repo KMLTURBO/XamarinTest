6fb4c7d41bbe40d09085e248735864a844ec2d31
Joe Sauve <joe.sauve@xamarin.com>
2016-05-24 21:38:19 -0500

    Enable XAMLC pre-compilation

--- Acquaint.XForms/Acquaint.XForms/App.xaml.cs
+++ Acquaint.XForms/Acquaint.XForms/App.xaml.cs
@@ -2,7 +2,7 @@
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
-//[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
 
 namespace Acquaint.XForms
 {
