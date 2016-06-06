187750ac9059116cee395a371144e05bb0b76302
Mike James <mike@xamarin.com>
2016-06-06 20:14:31 +0100

    Add Azure SDKs and updated data model

--- Acquaint.Data/Acquaintance.cs
+++ Acquaint.Data/Acquaintance.cs
@@ -1,12 +1,24 @@
 ﻿using System;
+using Microsoft.WindowsAzure.MobileServices;
 using MvvmHelpers;
-using Newtonsoft.Json;  
+using Newtonsoft.Json;
 
 namespace Acquaint.Data
 {
     public class Acquaintance : ObservableObject
     {
+        [JsonProperty(PropertyName = "id")]
         public string Id { get; set; }
+
+        [JsonProperty(PropertyName = "createdAt")]
+        public DateTimeOffset CreatedAt { get; set; }
+
+        [UpdatedAt]
+        public DateTimeOffset UpdatedAt { get; set; }
+
+        [Version]
+        public string AzureVersion { get; set; }
+
 
         string _FirstName;
         public string FirstName
